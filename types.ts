a6584d410794c62bc9475007d8723277a11ec57f
Varun Kishore <vk@Varuns-MacBook-Air.local>
2026-02-14 11:58:41 +0530

    Updated types file

--- types.ts
+++ types.ts
@@ -1,21 +1,25 @@
 export interface Product {
-  _id?: string;   // MongoDB id
-  id?: string;    // fallback if needed
-
+  _id?: string;   // from Mongo
+  id?: string;    // from constants (optional)
   name: string;
   slug: string;
   description: string;
   price: number;
   weight: string;
   image: string;
-
   nutritionalBenefits: string[];
   availability: string;
   growingTime: string;
-
   details: {
     healthBenefits: string;
     shelfLife: string;
     usage: string;
   };
+}
+
+
+export interface ContactForm {
+  name: string;
+  email: string;
+  message: string;
 }
